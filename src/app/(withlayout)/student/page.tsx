89c804fc1f9a44bb32aac48336ed8863b057cedc
DIDAR-HOSSAIN <didarhossain018@gmail.com>
2023-09-26 22:15:02 +0600

    first commit

---
+++ src/app/(withlayout)/student/page.tsx
@@ -0,0 +1,11 @@
+
+
+const StudentPage = () => {
+    return (
+        <div>
+            <h1>This page is student</h1>
+        </div>
+    );
+};
+
+export default StudentPage;
---
+++ src/app/(withlayout)/super-admin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SuperAdminPage = () => {
+    return (
+        <div>
+            <h1>This page is super Admin</h1>
+        </div>
+    );
+};
+
+export default SuperAdminPage;
